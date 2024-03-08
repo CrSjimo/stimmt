b2ba3fcd6810fdc6138118adae143b9d05080d4f
CrSjimo <crs_16423@outlook.com>
2024-03-09 01:11:18 +0800

    add module FileSystem

---
+++ src/module/FileSystem.cpp
@@ -0,0 +1,117 @@
+#include "FileSystem.h"
+
+#include <QDir>
+#include <QFile>
+#include <QJSEngine>
+
+#include <stimmt/ModuleSystem.h>
+#include <stimmt/TypedArrayHelper.h>
+#include <stimmt/ClosureHelper.h>
+
+namespace stimmt::modul {
+
+    FileSystem::FileSystem(QObject *parent) : QObject(parent) {
+    }
+
+    FileSystem::~FileSystem() = default;
+
+    void FileSystem::registerModule(ModuleSystem *moduleSystem) {
+        moduleSystem->registerModule("stimmt:filesystem", ClosureHelper::makeClosure(moduleSystem->engine(), new FileSystem));
+    }
+
+    QStringList FileSystem::readDir(const QString &dir) const {
+        return QDir().entryList();
+    }
+
+    bool FileSystem::exists(const QString &path) const {
+        return QDir().exists(path);
+    }
+
+    bool FileSystem::isDir(const QString &path) const {
+        return QFileInfo(path).isDir();
+    }
+
+    QJSValue FileSystem::readFile(const QString &path) const {
+        auto engine = qjsEngine(this);
+        Q_ASSERT(engine);
+        QFile f(path);
+        if (!f.open(QIODevice::ReadOnly)) {
+            engine->throwError("Cannot open file: " + path);
+            return {};
+        }
+        return TypedArrayHelper::toUint8Array(engine, f.readAll());
+    }
+
+    void FileSystem::writeFile(const QString &path, const QJSValue &data) const {
+        auto engine = qjsEngine(this);
+        Q_ASSERT(engine);
+        QFile f(path);
+        if (!f.open(QIODevice::WriteOnly)) {
+            engine->throwError("Cannot open file: " + path);
+            return;
+        }
+        auto byteData = data.isString() ? data.toString().toUtf8() : TypedArrayHelper::fromGenericValue(engine, data);
+        f.write(byteData);
+    }
+
+    void FileSystem::appendFile(const QString &path, const QJSValue &data) const {
+        auto engine = qjsEngine(this);
+        Q_ASSERT(engine);
+        QFile f(path);
+        if (!f.open(QIODevice::Append)) {
+            engine->throwError("Cannot open file: " + path);
+            return;
+        }
+        auto byteData = data.isString() ? data.toString().toUtf8() : TypedArrayHelper::fromGenericValue(engine, data);
+        f.write(byteData);
+    }
+
+    void FileSystem::remove(const QString &path) const {
+        auto engine = qjsEngine(this);
+        Q_ASSERT(engine);
+        if (!QFile::remove(path)) {
+            engine->throwError("Cannot remove: " + path);
+        }
+    }
+
+    void FileSystem::removeRecursively(const QString &dir) const {
+        auto engine = qjsEngine(this);
+        Q_ASSERT(engine);
+        if (!QDir(dir).removeRecursively()) {
+            engine->throwError("Cannot remove recursively: " + dir);
+        }
+    }
+
+    void FileSystem::rename(const QString &oldPath, const QString &newPath) const {
+        auto engine = qjsEngine(this);
+        Q_ASSERT(engine);
+        if (!QFile::rename(oldPath, newPath)) {
+            engine->throwError(QString("Cannot rename %1 to %2").arg(oldPath, newPath));
+        }
+    }
+
+    void FileSystem::copyFile(const QString &src, const QString &dest) const {
+        auto engine = qjsEngine(this);
+        Q_ASSERT(engine);
+        if (!QFile::copy(src, dest)) {
+            engine->throwError(QString("Cannot copy %1 to %2").arg(src, dest));
+        }
+    }
+
+    void FileSystem::mkdir(const QString &dir) const {
+        auto engine = qjsEngine(this);
+        Q_ASSERT(engine);
+        if (!QDir().mkdir(dir)) {
+            engine->throwError("Cannot mkdir: " + dir);
+        }
+    }
+
+    void FileSystem::mkpath(const QString &dir) const {
+        auto engine = qjsEngine(this);
+        Q_ASSERT(engine);
+        if (!QDir().mkpath(dir)) {
+            engine->throwError("Cannot mkpath: " + dir);
+        }
+    }
+
+} // stimmt::modul
---
+++ src/module/FileSystem.h
@@ -0,0 +1,45 @@
+#ifndef STIMMT_FILESYSTEM_H
+#define STIMMT_FILESYSTEM_H
+
+#include <QJSValue>
+#include <QObject>
+
+#include <stimmt/stimmtGlobal.h>
+
+class QJSEngine;
+
+namespace stimmt {
+    class ModuleSystem;
+}
+
+namespace stimmt::modul {
+
+    class STIMMT_EXPORT FileSystem : public QObject {
+        Q_OBJECT
+    public:
+        explicit FileSystem(QObject *parent = nullptr);
+        ~FileSystem() override;
+
+        static void registerModule(ModuleSystem *moduleSystem);
+
+    public slots:
+        QStringList readDir(const QString &dir) const;
+        bool exists(const QString &path) const;
+        bool isDir(const QString &path) const;
+
+        QJSValue readFile(const QString &path) const;
+        void writeFile(const QString &path, const QJSValue &data) const;
+        void appendFile(const QString &path, const QJSValue &data) const;
+
+        void remove(const QString &path) const;
+        void removeRecursively(const QString &dir) const;
+        void rename(const QString &oldPath, const QString &newPath) const;
+        void copyFile(const QString &src, const QString &dest) const;
+
+        void mkdir(const QString &dir) const;
+        void mkpath(const QString &dir) const;
+    };
+
+} // stimmt::modul
+
+#endif //STIMMT_FILESYSTEM_H
